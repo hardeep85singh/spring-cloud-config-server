bfbce0e980e13381002b9764352d14fa3fad8250
hardeepacuver <hardeep.singh@acuverconsulting.com>
2022-03-22 13:19:41 +0530

    First Commit

---
+++ src/main/java/in/projects/hard/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package in.projects.hard.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
